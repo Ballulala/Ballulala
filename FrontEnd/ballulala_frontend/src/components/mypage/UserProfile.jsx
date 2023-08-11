3ea4d8136d2e046eadac4cef53be61ca5fc0cba0
김상진 <tkdwls0418@naver.com>
2023-08-11 16:34:57 +0900

    update: 마이페이지 수정

--- FrontEnd/ballulala_frontend/src/components/mypage/UserProfile.jsx
+++ FrontEnd/ballulala_frontend/src/components/mypage/UserProfile.jsx
@@ -1,66 +1,27 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 import "./UserProfile.css";
 import UserStats from "./UserStats";
 import TeamInfo from "./TeamInfo";
 import UserPoints from "./UserPoints";
-import UserInfo from "./UserInfo";
+import UserInfo from "./UserInfo"; // 새로 추가된 줄입니다.
 
-function UserProfile() {
-  const [user, setUser] = useState(null);
-  const calculateAge = (birthdayStr) => {
-    const birthYear = parseInt(birthdayStr.split("-")[0], 10);
-    const currentYear = new Date().getFullYear();
-    return currentYear - birthYear;
-  };
-
-  useEffect(() => {
-    const fetchUserInfo = async () => {
-      try {
-        const response = await axios.get(
-          "https://i9d110.p.ssafy.io:8081/users/myInfo",
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
-          }
-        );
-
-        if (response.data.state === "SUCCESS") {
-          setUser(response.data.user);
-        }
-      } catch (error) {
-        console.error("유저 정보 불러오기 실패", error);
-      }
-    };
-
-    fetchUserInfo();
-  }, []);
-
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
+function UserProfile({ user }) {
   return (
     <div className="user-profile">
       <div className="left-section">
-        <p>User logo</p>
-        <img
-          src={user.profileImage || "default_image_url"}
-          alt="User profile"
-        />
-        <h2>{user.nickname}</h2>
+        <img src={user.profilePicture} alt="User profile" />
+        <h2>{user.name}</h2>
         <UserInfo
-          age={calculateAge(user.birthday)}
+          age={user.age}
           gender={user.gender}
-          location={`${user.sido || ""} ${user.gugun || ""}`}
+          location={user.location}
           email={user.email}
         />
       </div>
       <div className="right-section">
-        <TeamInfo teamLogo={user.profileImage} teamName={user.nickname} />
-        <UserStats mvpCount={user.mvpCount} mannerScore={user.manner} />
-        <UserPoints points={user.point} />
+        <TeamInfo teamLogo={user.teamLogo} teamName={user.teamName} />
+        <UserStats mvpCount={user.mvpCount} mannerScore={user.mannerScore} />
+        <UserPoints points={user.points} />
       </div>
     </div>
   );
