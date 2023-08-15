89575ea5530de58e2844029d4892651aee3d4fb6
jeonghwanKim <doyun30@naver.com>
2023-08-15 20:36:29 +0900

    Add. 구장 더미데이터 추가

--- BackEnd/src/main/resources/import.sql
+++ BackEnd/src/main/resources/import.sql
@@ -48,7 +48,22 @@ insert into item(cost, img, name) values(5000, "웃는 얼굴", "웃는 얼굴")
 insert into item(cost, img, name) values(10000, "웃는 얼굴", "웃는 얼굴");
 
 -- 1번 구장, 이름 haha1
-insert into stadium(stadium_id, description,name,phone_number,region)values(1,"ㅎㅎ","haha1", "haha", 0);
+insert into stadium(escription,name,phone_number,region)values("서울스다디움","서울스다디움", "haha", 0);
+insert into stadium(description,name,phone_number,region)values("경기스다디움","경기스다디움", "haha", 1);
+insert into stadium(description,name,phone_number,region)values("인천스다디움","인천스다디움", "haha", 2);
+insert into stadium(description,name,phone_number,region)values("강원스다디움","강원스다디움", "haha", 3);
+insert into stadium(description,name,phone_number,region)values("대구스다디움","대구스다디움", "haha", 4);
+insert into stadium(description,name,phone_number,region)values("대전스다디움","대전스다디움", "haha", 5);
+insert into stadium(description,name,phone_number,region)values("경남스다디움","경남스다디움", "haha", 6);
+insert into stadium(description,name,phone_number,region)values("경북스다디움","경북스다디움", "haha", 7);
+insert into stadium(description,name,phone_number,region)values("부산스다디움","부산스다디움", "haha", 8);
+insert into stadium(description,name,phone_number,region)values("울산스다디움","울산스다디움", "haha", 9);
+insert into stadium(description,name,phone_number,region)values("광주스다디움","광주스다디움", "haha", 10);
+insert into stadium(description,name,phone_number,region)values("제주스다디움","제주스다디움", "haha", 11);
+insert into stadium(description,name,phone_number,region)values("전남스다디움","전남스다디움", "haha", 12);
+insert into stadium(description,name,phone_number,region)values("전북스다디움","전북스다디움", "haha", 13);
+insert into stadium(description,name,phone_number,region)values("충남스다디움","충남스다디움", "haha", 14);
+insert into stadium(description,name,phone_number,region)values("충북스다디움","충북스다디움", "haha", 15);
 
 -- 2023-08-08
 insert into matches(match_time, state, match_date, stadium_id, team_id1, team_id2, team_id3) values(2,0,"2023-08-08",1,1,2,3);
