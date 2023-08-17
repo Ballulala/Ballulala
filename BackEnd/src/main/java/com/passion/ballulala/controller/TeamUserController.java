657f3fa2f804bca86aad5a5f8734ac1ca24bf454
DESKTOP-DOGVPUB\SSAFY <corudgh123@naver.com>
2023-08-17 15:26:32 +0900

    .Feat 가입 거절

--- BackEnd/src/main/java/com/passion/ballulala/controller/TeamUserController.java
+++ BackEnd/src/main/java/com/passion/ballulala/controller/TeamUserController.java
@@ -90,4 +90,20 @@ public class TeamUserController {
         return new ResponseEntity<Map<String, Object>>(resultMap, status);
     }
 
+    @GetMapping("/joinDenied/{teamId}")
+    public ResponseEntity<Map<String, Object>> joinDenied(@PathVariable("teamId") Long teamId, HttpServletRequest request) {
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = null;
+        try {
+            teamUserService.memberOut(teamId, request.getHeader("Authorization"));
+            resultMap.put("message", "success");
+            status = HttpStatus.ACCEPTED;
+        } catch (Exception e) {
+            resultMap.put("message", "fail: " + e.getClass().getSimpleName());
+            status = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+
 }
